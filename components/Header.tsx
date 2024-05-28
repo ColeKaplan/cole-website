4566bb0f75bc708501cb982a3042f7a9858fb998
ColeKaplan <colegkaplan@gmail.com>
2024-05-28 17:39:38 -0400

    set up navbar links between pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import './Header.css';
 
 function Header() {
+  const [activeLink, setActiveLink] = useState<string>('');
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+    setTimeout(() => setActiveLink(''), 1000); // Remove blink effect after 1 second
+  };
+
   return (
-    <>
-      <h1>Header</h1>
-    </>
-  )
+    <div id="nav-bar">
+      <nav className='nav-header'>
+        {['home', 'about', 'games', 'resume', 'contact'].map((page, index) => (
+          <>
+            <Link href={`/${page === 'home' ? '' : page}`} key={page}
+                className={`nav-link ${activeLink === page ? 'blink' : ''}`}
+                onClick={() => handleLinkClick(page)}
+              >
+                {page.charAt(0).toUpperCase() + page.slice(1)}
+            </Link>
+            {index !== ['home', 'about', 'games', 'resume', 'contact'].length - 1 && <p className="nav-slash">|</p>}
+          </>
+        ))}
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
