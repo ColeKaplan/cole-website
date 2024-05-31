056163c9534706031b49b93fcc9db6d2497804fc
ColeKaplan <colegkaplan@gmail.com>
2024-05-31 09:01:17 -0400

    hopefully fixes prod, addded type declaration to props

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,12 +6,12 @@ const montserrat = Montserrat({
     weight: ['400', '500', '700', '900'],
   });
 
-function Card(props) {
+function Card(props: any) {
 
     return (
         <button className='bg-[#fffce4] border-gray-200 border-2 rounded-xl shadow-lg p-3 m-3 text-center
         active:bg-[#fef8c9] active:text-black max-w-60'>
-            <img className='rounded-md mb-4 max-w-max' src={props.image} width={210} alt="profile picture"></img>
+            <img className='rounded-md mb-4 max-w-max' src={props.image} width={200} alt="profile picture"></img>
             <h2 className={`${montserrat} font-black text-2xl`}>{props.title}</h2>
             <p className={`text-sm`}>{props.text}</p>
         </button>
