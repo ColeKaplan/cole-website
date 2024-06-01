5bf24727be26aa4e06054d800c8c58e603149fbb
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2024-05-31 20:13:22 -0400

    added images for each game and iframe no longer scrolls

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,58 @@ import React from 'react';
 
 export default function Home(){
     return(
-        <div className='flex'>
-            <div>
-                <Card 
-                link='/WildRanger'
-                image='/games/WildRanger/bank_inside.png'
-                title="Wild Ranger"
-                text="Cowboy themed Shoot 'Em Up' with a live leaderboard"
-                />
+        <div className='flex flex-col items-center justify-center'>
+            <div className='flex items-center justify-center'>
+                <div>
+                    <Card 
+                    link='/WildRanger'
+                    image='/games/WildRanger/BankImage.png'
+                    title="Wild Ranger"
+                    text="Cowboy themed Shoot 'Em Up' with a live leaderboard"
+                    />
+                </div>
+                <div>
+                    <Card 
+                    link='/KnightsJourney'
+                    image='/games/KnightsJourney/KnightImage.png'
+                    title="Knight's Journey"
+                    text="Dungeon Crawler, try to get past the enemies"
+                    />
+                </div>
+                <div>
+                    <Card 
+                    link='/FishOnLand'
+                    image='/games/FishOnLand/FishImage.png'
+                    title="Fish on Land"
+                    text="Help a lost fish return to his home in the ocean"
+                    />
+                </div>
             </div>
-            <div>
-                <Card 
-                link='/KnightsJourney'
-                image='/games/WildRanger/bank_inside.png'
-                title="Knight's Journey"
-                text="Dungeon Crawler with advanced movement and animations"
-                />
-            </div>
-            <div>
-                <Card 
-                link='/FishOnLand'
-                image='/games/WildRanger/bank_inside.png'
-                title="Fish on Land"
-                text="Help a lost fish return to his home in the ocean"
-                />
+            <div className='flex items-center justify-center'>
+                <div>
+                    <Card 
+                    link='/WildRanger'
+                    image='/games/WildRanger/BankImage.png'
+                    title="Wild Ranger"
+                    text="Cowboy themed Shoot 'Em Up' with a live leaderboard"
+                    />
+                </div>
+                <div>
+                    <Card 
+                    link='/KnightsJourney'
+                    image='/games/KnightsJourney/KnightImage.png'
+                    title="Knight's Journey"
+                    text="Dungeon Crawler, try to get past the enemies"
+                    />
+                </div>
+                <div>
+                    <Card 
+                    link='/FishOnLand'
+                    image='/games/FishOnLand/FishImage.png'
+                    title="Fish on Land"
+                    text="Help a lost fish return to his home in the ocean"
+                    />
+                </div>
             </div>
         </div>
     );
--- components/UnityGame.tsx
+++ components/UnityGame.tsx
@@ -3,10 +3,10 @@
 export default function UnityGame(props: any) {
 
     return(
-        <div className="flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center flex-col overflow-x-hidden">
             <h1>{props.title}</h1>
-            <iframe src={props.build} width="960" height="640" 
-            className=''/>
+            <iframe src={props.build} width="980" height="660" 
+            className='overflow-hidden'/>
         </div>
     );
 
