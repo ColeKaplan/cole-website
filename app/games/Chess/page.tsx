7d2df426e0f0984031adc5a53770d5aa32ba6743
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2024-06-12 23:03:24 -0400

    starting working on chess and ai

--- app/games/Chess/page.tsx
+++ app/games/Chess/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ChessSquare from '@/components/ChessComponents/ChessSquare';
 
 export default function Chess(){
     return(
         <div className='mt-16'>
             <div>
-                <p>Chess</p>
+                <ChessSquare 
+                position='e1'
+                />
             </div>
         </div>
     );
 }
+
---
+++ components/AIGame.tsx
@@ -0,0 +1,6 @@
+import * as tf from '@tensorflow/tfjs';
+
+const model = await tf.loadLayersModel('https://foo.bar/tfjs_artifacts/model.json');
+
+const example = tf.fromPixels(webcamElement);
+const prediction = model.predict(example);
