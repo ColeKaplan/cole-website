4f6d76647ff14d3e87a07398c7a0295f2cda9399
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2024-07-13 22:06:07 -0400

    omg I think I know how to make the AI project work with PyScript

--- app/games/DrawingGame/page.tsx
+++ app/games/DrawingGame/page.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default function DrawingGame(){
     return(
         <div className='mt-16'>
-            <div>
-                <p>Drawing</p>
+            <div className="flex items-center justify-center flex-col overflow-x-hidden">
+                <iframe src="/games/DrawingGame/index.html" 
+                width="500" 
+                height="500"
+                className=''/>
             </div>
         </div>
     );
