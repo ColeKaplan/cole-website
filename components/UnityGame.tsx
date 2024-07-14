758872207ca0fbb1fb684cff2185e1119197143c
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2024-07-13 20:44:56 -0400

    hopefully fixes the build problem with window

--- components/UnityGame.tsx
+++ components/UnityGame.tsx
@@ -2,33 +2,39 @@
 
 import { useState, useEffect } from 'react';
 
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height
-    };
-}
-
 
 export default function UnityGame(props: any) {
+
+    const hasWindow = typeof window !== 'undefined';
+
+    function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+        width,
+        height,
+        };
+    }
 
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
     useEffect(() => {
-        function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-        }
+        if (hasWindow) {
+            const handleResize = () => {
+                setWindowDimensions(getWindowDimensions());
+            }
 
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, [hasWindow]);
+
 
     return(
         <div className="flex items-center justify-center flex-col overflow-x-hidden">
             <h1 className='text-4xl font-bold text-[#FFFFFF] pb-4'>{props.title}</h1>
             <iframe src={props.build} 
-            width={windowDimensions.width * .75} 
+            width={windowDimensions.width != null ? windowDimensions.width * .75 : props.width} 
             height={props.height}  
             className='overflow-hidden'/>
         </div>
