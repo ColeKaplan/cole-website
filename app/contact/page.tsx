afeb6eb71c7f432f36b2366686dcdbd0532b731a
ColeKaplan <colegkaplan@gmail.com>
2024-11-26 17:30:08 -0500

    small change coloring

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Contact(){
     return(
         <div>
             <div className='flex flex-col justify-items-start items-center min-h-screen py-10 bg-[#slate-500]'>
-                <h1 className='text-4xl font-bold text-[#dfdfdf]'>Contact Me</h1>
+                <h1 className='text-4xl font-bold text-[#ffffff]'>Contact Me</h1>
                 <ContactForm/>
             </div>
         </div>
