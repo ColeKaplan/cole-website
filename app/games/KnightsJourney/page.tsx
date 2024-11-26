e0273fa5133055e072602063407481b3141a9dea
ColeKaplan <colegkaplan@gmail.com>
2024-11-26 17:21:44 -0500

    new front page. Recognition and links to friends for game jams + quick summaries

--- app/games/KnightsJourney/page.tsx
+++ app/games/KnightsJourney/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function KnightsJourney(){
     return(
         <div className='bg-slate-500 h-auto pt-10'>
-            <div>
+            <div className='flex flex-col items-center'>
                 <UnityGame
                 build="/games/KnightsJourney/index.html"
                 title="Knight's Journey"
@@ -13,6 +13,13 @@ export default function KnightsJourney(){
                 heightPercent=".85"
                 ratio=".6"
                 />
+            
+                <p className='text-[#ffffff] pt-1'>Creators: 
+                    <a href="https://www.linkedin.com/in/benjaminchern/" className={`hover:text-[#A0BEE6]`}> Benjamin Chern</a>, 
+                    Cole Kaplan
+                </p>
+                <p className='text-[#ffffff]'>I made Knight's Journey for a forgotten game jam in 2021</p>
+                <p className='text-[#ffffff]'>The focus of this game was to improve my skills in simulated physics, character animations, and collision calculations</p>
             </div>
         </div>
     );
--- app/games/WildRanger/page.tsx
+++ app/games/WildRanger/page.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function WildRanger(){
     return(
-        <div className='flex flex-col justify-items-start items-center h-auto pt-10 bg-slate-500'>
-            <div>
+        <div className='h-auto pt-10 bg-slate-500'>
+            <div className='flex flex-col items-center'>
                 <UnityGame
                 build="/games/WildRanger/index.html"
                 title="Wild Ranger"
@@ -13,6 +13,15 @@ export default function WildRanger(){
                 heightPercent=".95"
                 ratio=".5"
                 />
+            
+                <p className='text-[#ffffff] pt-1'>Creators: 
+                    <a href="https://www.linkedin.com/in/benjaminchern/" className={`hover:text-[#A0BEE6]`}> Benjamin Chern</a>, 
+                    <a href="https://jackwarren430.github.io/" className={`hover:text-[#A0BEE6]`}> Jack Warren</a>, 
+                    <a href="https://www.jadenh.dev/" className={`hover:text-[#A0BEE6]`}> Jaden Hamer</a>, and
+                    Cole Kaplan
+                </p>
+                <p className='text-[#ffffff]'>I made Wild Ranger for ScoreSpace Jam #26 in 2023</p>
+                <p className='text-[#ffffff]'>I am most proud of it's complete playability and leaderboard</p>
             </div>
         </div>
     );
