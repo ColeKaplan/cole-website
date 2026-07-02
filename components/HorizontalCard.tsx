52e3d3ec085552088c2b3c2de69f69b1533d4aa4
ColeKaplan <colegkaplan@gmail.com>
2026-07-01 20:27:21 -0700

    pointer on hover

--- components/HorizontalCard.tsx
+++ components/HorizontalCard.tsx
@@ -20,7 +20,7 @@ function HorizontalCard(props: any) {
         <div /* Should be an <a> tag but it doesn't let me nest <a> tags */
             onClick={() => router.push(props.link)} className='bg-[#c2e2f9] border-2 rounded-xl shadow-xl p-3 m-3 text-center
         active:bg-[#82cbff] active:text-black hover:bg-[#9fd7ff] hover:text-black max-w-xl
-        grow'>
+        grow cursor-pointer'>
             <div className={`flex ${isTextRight ? 'flex-row-reverse' : 'flex-row'} items-center`}>
                 <div className="">
                     <Image className='rounded-md max-w-60 max-h-40' src={props.image} width={420} height={320} alt={`${props.title} picture`} priority={true}></Image>
