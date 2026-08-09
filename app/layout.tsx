0cbdd4e9216a4879ff5113e5cf5625ede1f4e2da
ColeKaplan <colegkaplan@gmail.com>
2026-08-08 22:42:02 -0700

    next/scrtipt for umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +22,15 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-
-        <script defer src="https://stats.colekaplan.dev/script.js" data-website-id="506e9d06-ba8e-41e9-acc1-7f8a670090eb"></script>
       </head>
       <body className={`${inter.className} bg-slate-500 overscroll-none  text-[#000000]`}>
         
         {children}
+        <Script
+          src="https://stats.colekaplan.dev/script.js"
+          data-website-id="506e9d06-ba8e-41e9-acc1-7f8a670090eb"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
